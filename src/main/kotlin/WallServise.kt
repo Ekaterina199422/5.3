5ce142982d8475492c5db464a32b5e84c3b1847d
katerina kulchitskay <rfvb.foto@mail.ru>
2020-10-26 11:38:44 +0200

    first commit

---
+++ src/main/kotlin/WallServise.kt
@@ -0,0 +1,24 @@
+class WallService {
+    private var postsArray = emptyArray<Post>()
+    private var idPost = 0
+
+    fun add(post: Post): Post {
+        idPost += 1
+        post.id = idPost
+        postsArray += post
+        return postsArray.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, posts) in postsArray.withIndex()) {
+            if (post.id == posts.id) {
+                postsArray[index] = post.copy(
+                    ownerId = posts.ownerId,
+                    date = posts.date
+                )
+                return true
+            }
+        }
+        return false
+    }
+}
